61e5a194705e9ef5614da2ea9829bbf83f924f57
Godagesasindu <godagesasindu@gmail.com>
2024-10-20 20:50:33 +0530

    initial commit

---
+++ src/main/java/org/smilegirl/smilegirl_web_backend/SmileGirlWebBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.smilegirl.smilegirl_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileGirlWebBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmileGirlWebBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/smilegirl/smilegirl_web_backend/SmileGirlWebBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smilegirl.smilegirl_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileGirlWebBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
